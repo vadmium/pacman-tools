7179a1c30eab3df2553e357fb69bdb92f92f0eca
Martin Panter <vadmium  gmailcom>
2014-02-12 09:49:20 +0000

    Move Dynamic class into “pyelftools”

--- deps.py
+++ deps.py
@@ -34,7 +34,7 @@ class Deps(object):
                 yield seg.get_interp_name()
     
     def needed(self):
-        for tag in self.dynamic.eliben.iter_tags('DT_NEEDED'):
+        for tag in self.dynamic.iter_tags('DT_NEEDED'):
             entry = tag.needed
             name = self.sub_origin(entry)
             yield dict(search=b"/" not in name, name=name, raw_name=entry)
@@ -47,8 +47,8 @@ class Deps(object):
                 yield filename
     
     def search_dirs(self, config_dirs):
-        if not any(self.dynamic.eliben.iter_tags('DT_RUNPATH')):
-            for dirs in self.dynamic.eliben.iter_tags('DT_RPATH'):
+        if not any(self.dynamic.iter_tags('DT_RUNPATH')):
+            for dirs in self.dynamic.iter_tags('DT_RPATH'):
                 for dir in dirs.rpath.split(b":"):
                     # $ORIGIN substitution in DT_RPATH string seen in Libre
                     # Office 3.6.2's libmozbootstrap.so file. The Gnu "ldd"
@@ -72,7 +72,7 @@ class Deps(object):
                     for dir in dirs.split(b":"):
                         yield dir.lstrip(b"/")
         
-        for dirs in self.dynamic.eliben.iter_tags('DT_RUNPATH'):
+        for dirs in self.dynamic.iter_tags('DT_RUNPATH'):
             for dir in dirs.runpath.split(b":"):
                 try:
                     dir = self.sub_origin(dir)
